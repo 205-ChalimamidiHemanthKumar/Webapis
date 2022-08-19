e2a50db3b6018f83c4e03c039b75ed8ba960b01c
mindedge26 <mindedge26@MEDGE164>
2022-08-19 10:23:10 +0530

    Add project files.

---
+++ Webapis/Data/WebapisContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Webapis.Models;
+
+namespace Webapis.Data
+{
+    public class WebapisContext : DbContext
+    {
+        public WebapisContext (DbContextOptions<WebapisContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Webapis.Models.Employees> Employees { get; set; }
+    }
+}
---
+++ Webapis/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapis.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmpName { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+    }
+}
